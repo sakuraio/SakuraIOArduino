170ad7af23f0fe6745f779440f7d21acc3baae71
Yuuichi Akagawa <yuuichi.akagawa@gmail.com>
2016-12-17 11:42:38 +0900

    Improve connectivity with multiple SPI devices

--- src/SakuraIO_SPI.cpp
+++ src/SakuraIO_SPI.cpp
@@ -2,8 +2,14 @@
 #include "SakuraIO.h"
 #include "SakuraIO/debug.h"
 
+#if SPI_HAS_TRANSACTION
+static SPISettings settings;
+#endif
 
 void SakuraIO_SPI::begin(){
+#if SPI_HAS_TRANSACTION
+  SPI.beginTransaction(settings);
+#endif
   dbgln("CS=0");
   digitalWrite(cs, LOW);
 }
@@ -11,6 +17,9 @@ void SakuraIO_SPI::begin(){
 void SakuraIO_SPI::end(){
   dbgln("CS=1");
   digitalWrite(cs, HIGH);
+#if SPI_HAS_TRANSACTION
+  SPI.endTransaction();
+#endif
   delayMicroseconds(20);
 }
 
@@ -38,6 +47,9 @@ uint8_t SakuraIO_SPI::receiveByte(){
 SakuraIO_SPI::SakuraIO_SPI(int _cs){
   cs = _cs;
   SPI.begin();
+#if SPI_HAS_TRANSACTION
+  settings = SPISettings(4000000, MSBFIRST, SPI_MODE0); // 4MHz, MSB First, SPI mode 0
+#endif
   pinMode(cs, OUTPUT);
   digitalWrite(cs, HIGH);
 }
